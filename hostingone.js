aa2c591f1502361d0409f2919bb50f72aee83a9c
SupaProgrammer <ddaniels.science1@gmail.com>
2024-04-17 11:44:37 -0500

    initialize git repository

---
+++ hostingone.js
@@ -0,0 +1,12 @@
+
+for(let i = 1; i <= 20; i++){
+    if(i % 3 === 0 && i % 5 === 0)
+    console.log(`${i} => Frontend Simplified`)
+    else if (i % 3 === 0){
+        console.log(`${i} Frontend`)
+    }
+    else if (i % 5 === 0){
+        console.log(`${i} Simplified`)
+    }
+console.log((i))
+}
